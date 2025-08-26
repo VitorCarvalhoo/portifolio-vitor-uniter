87a8fabacf195d4bda1459a540f4f7beeae5a0e5
Vitor G. Carvalho <78125536+VitorCarvalhoo@users.noreply.github.com>
2025-08-25 22:30:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 document.getElementById("enviar-form").addEventListener("submit", function(event) { // Adiciona um ouvinte de evento ao botão de envio do formulário
     event.preventDefault(); // Impede o envio padrão do formulário
 
-    // Coleta os valores dos campos do formulário
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const assunto = document.getElementById("assunto").value;
-    const mensagem = document.getElementById("mensagem").value; 
-
-    // Validação simples dos campos
-    if (nome === "" || email === "" || assunto === "" || mensagem === "") {
-        alert("Por favor, preencha todos os campos.");
-        return;
-    }
-
     // Exibe uma mensagem de confirmação
     showToast("Obrigado por entrar em contato! Responderemos em breve.");
 
-    // Limpa os campos do formulário
-    document.getElementById("contato-form").reset(); // Reseta o formulário
+
 });
 
 function showToast(message) { // Função para exibir uma mensagem tipo "toast" (Temporaria)
@@ -29,3 +16,4 @@ function showToast(message) { // Função para exibir uma mensagem tipo "toast" 
     }, 3000);
 
 }
+
